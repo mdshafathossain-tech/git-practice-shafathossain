9bd0938cc8caf59c47bc5bd0cf59737cf215d241
Md. Shafat Hossain <mdshafathossain@gmail.com>
2026-05-01 14:43:32 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import datetime
+from utils import add, subtract
 
 name = "Shafat Hossain"
 today = datetime.date.today()
 
 print(f"Hello, my name is {name}")
 print(f"Today's date is: {today}")
+
+# Testing the new calculator functions
+print(f"10 + 5 = {add(10, 5)}")
+print(f"10 - 5 = {subtract(10, 5)}")
